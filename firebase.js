caf01dedd6b80bbc070499687991bcb9c6300629
CORENTIN ROSSETTO <114078357+Rococo77@users.noreply.github.com>
2024-03-18 16:15:58 +0100

    bundle installe

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADInlxbsoEsrApJ6J1DLOZ5DHvK4Oe2dQ",
+  authDomain: "ugrademareact.firebaseapp.com",
+  projectId: "ugrademareact",
+  storageBucket: "ugrademareact.appspot.com",
+  messagingSenderId: "835921943154",
+  appId: "1:835921943154:web:972c2f885ba7be7e4800eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+export {app, db, auth}
